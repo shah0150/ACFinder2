c852d7102066796cbff830a3b6304227034db0a2
shah0150 <shah0150@algonquinlive.com>
2016-11-18 12:42:09 -0500

    Algonquin College Finder

---
+++ app/src/main/java/com/shah0150algonquinlive/acfinder/Constants.java
@@ -0,0 +1,11 @@
+package com.shah0150algonquinlive.acfinder;
+
+/**
+ * Created by adeshshah on 2016-11-18.
+ */
+
+public interface Constants {
+    public static final String PEMBROKE = "Algonquin College in the Valley";
+    public static final String PERTH = "7 Craig Street, Perth, ON, K7H 1X7";
+    public static final String WOODROFFE = "1385 Woodroffe Avenue, Ottawa, ON, K2G 1V8";
+}
---
+++ app/src/main/java/com/shah0150algonquinlive/acfinder/MapsActivity.java
@@ -0,0 +1,91 @@
+package com.shah0150algonquinlive.acfinder;
+
+import android.location.Address;
+import android.location.Geocoder;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+/**
+ * Pin each campus of Algonquin College on a Google map.
+ *
+ * @author Adesh Shah (shah0150)
+ * @version 2.0
+ *
+ */
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, Constants {
+
+    private GoogleMap mMap;
+    private Geocoder mGeocoder;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        mGeocoder = new Geocoder( this );
+
+        final EditText userLocation = (EditText) findViewById(R.id.userLocation);
+        userLocation.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_NULL && event.getAction() == KeyEvent.ACTION_DOWN && event.getKeyCode() == KeyEvent.KEYCODE_ENTER) {
+                    pin(userLocation.getText().toString());
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+//        // Add a marker in Sydney and move the camera
+//        LatLng sydney = new LatLng(-34, 151);
+//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // pin each of Algonquin College's campuses to the map
+        pin( PEMBROKE );
+        pin(PERTH);
+        pin(WOODROFFE);
+    }
+    /** Locate and pin locationName to the map. */
+    private void pin( String locationName ) {
+        try {
+            Address address = mGeocoder.getFromLocationName(locationName, 1).get(0);
+            LatLng ll = new LatLng( address.getLatitude(), address.getLongitude() );
+            mMap.addMarker( new MarkerOptions().position(ll).title(locationName) );
+            mMap.moveCamera( CameraUpdateFactory.newLatLng(ll) );
+            Toast.makeText(this, "Pinned: " + locationName, Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            Toast.makeText(this, "Not found: " + locationName, Toast.LENGTH_SHORT).show();
+        }
+    }
+}
